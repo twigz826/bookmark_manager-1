a0bdf0bb2531e14d2a08ea57115d47cbc0aca410
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-28 18:27:34 +0000

    Added deleting_bookmark_spec.rb. This reflects progress on Deleting Bookmarks.

---
+++ spec/features/deleting_bookmark_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# feature 'Deleting a bookmark' do
+#   scenario 'user adds three bookmarks and deletes the second' do
+#     add_bookmark(SITE_BBC_NEWS_URL, SITE_BBC_NEWS_TITLE)
+#     add_bookmark(SITE_GOOGLE_URL, SITE_GOOGLE_TITLE)
+#     add_bookmark(SITE_MAKERS_URL, SITE_MAKERS_TITLE)
+#
+#     @con.exec("DELETE FROM bookmarks WHERE title=#{SITE_GOOGLE_TITLE}")
+#
+#
+#   end
+# end
