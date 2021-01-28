e739266fd33e95b4836fec6c585b3ec7ebd596fd
twigz826 <thomastwigden@gmail.com>
2021-01-28 12:53:03 -0300

    Refactored self.all to create an array and refactored tests

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,10 +4,18 @@ require 'pg'
 
 # Each bookmark is an instance of the Bookmark class.
 class Bookmark
+  attr_reader :id, :title, :url
+
   @con = if ENV['ENVIRONMENT'] == 'test'
            PG.connect dbname: 'bookmark_manager_test'
          else PG.connect dbname: 'bookmark_manager'
          end
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end
 
   def self.add(new_url, new_title)
     @con.exec "INSERT INTO bookmarks (url, title) VALUES('#{new_url}', '#{new_title}')"
@@ -15,6 +23,6 @@ class Bookmark
 
   def self.all
     rs = @con.exec 'SELECT * FROM bookmarks'
-    rs.map { |bookmark| { title: bookmark['title'], url: bookmark['url'] } }
+    rs.map { |bookmark| Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url']) }
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,20 +7,20 @@ describe Bookmark do
     it 'returns all bookmarks' do
       con = PG.connect dbname: 'bookmark_manager_test'
 
-      con.exec("INSERT INTO bookmarks (url, title) VALUES ('#{SITE_BBC_NEWS_URL}', '#{SITE_BBC_NEWS_TITLE}');")
+      con.exec("INSERT INTO bookmarks (url, title) VALUES('#{SITE_BBC_NEWS_URL}', '#{SITE_BBC_NEWS_TITLE}');")
       con.exec("INSERT INTO bookmarks (url, title) VALUES('#{SITE_GOOGLE_URL}', '#{SITE_GOOGLE_TITLE}');")
       con.exec("INSERT INTO bookmarks (url, title) VALUES('#{SITE_MAKERS_URL}', '#{SITE_MAKERS_TITLE}');")
 
-      expect(Bookmark.all[0][:title]).to include(SITE_BBC_NEWS_TITLE)
-      expect(Bookmark.all[1][:title]).to include(SITE_GOOGLE_TITLE)
-      expect(Bookmark.all[2][:title]).to include(SITE_MAKERS_TITLE)
+      expect(Bookmark.all[0].title).to include(SITE_BBC_NEWS_TITLE)
+      expect(Bookmark.all[1].title).to include(SITE_GOOGLE_TITLE)
+      expect(Bookmark.all[2].title).to include(SITE_MAKERS_TITLE)
     end
   end
 
   describe '.add' do
     it 'adds a bookmark to the database' do
       Bookmark.add(SITE_HACKER_TYPER_URL, SITE_HACKER_TYPER_TITLE)
-      expect(Bookmark.all[0][:title]).to include(SITE_HACKER_TYPER_TITLE)
+      expect(Bookmark.all[0].title).to include(SITE_HACKER_TYPER_TITLE)
     end
   end
 end
